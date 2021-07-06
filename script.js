9cf63277d8bb7e1cea823e8b45385b9efc58bdde
Gabriel Allba <rielgasy9@gmail.com>
2021-07-06 20:49:37 +0700

    add quotes

--- script.js
+++ script.js
@@ -9,22 +9,96 @@ const zodQuote = {
     happiness: [10,20,30,40,50,60,5,7,8,9,12,15,25,35,44,28],
         
     mondayQuotes: [
-        '“The purpose of our lives is to be happy.” — Dalai Lama',
-        '“Life is what happens when you’re busy making other plans.” — John Lennon',
-        '“Get busy living or get busy dying.” — Stephen King'
+        '“The best time to plant a tree was 20 years ago. The second best time is now.” — Chinese proverb',
+        '“I attribute my success to this: I never gave or took any excuse.” — Florence Nightingale',
+        '“Do it or not. There is no try.” — Yoda',
+        '“Twenty years from now, you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails. Explore, Dream, Discover.” — Mark Twain',
+        '“The most common way people give up their power is by thinking they don’t have any.” — Alice Walker',
+        '“Life is what happens to you while you’re busy making other plans.” — John Lennon',
+        '“It is never too late to be what you might have been.” — George Eliot',
+        '“If you want to lift yourself up, lift up someone else.” — Booker T. Washington',
+        '“Your time is limited, so don’t waste it living someone else’s life.” — Steve Jobs',
+        '“Whatever you can do, or dream you can, begin it. Boldness has genius, power and magic in it.” — Johann Wolfgang von Goethe',
+        '“Whatever the mind of man can conceive and believe, it can achieve.” – Napoleon Hill',
+        '“Life offers you so many doors, it is up to you which to open and which one to close.” – Unknown',
+        '“The man who has confidence in himself gains the confidence of others.” – Hasidic Proverb',
+        '“If not us, who? If not now, when?” – John F. Kennedy',
+        '“The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.” – Mark Caine',
+        '“Excellence is an art won by training and habituation. We do not act rightly because we have virtue or excellence, but we rather have those because we have acted rightly. We are what we repeatedly do. Excellence, then, is not an act but a habit.” – Aristotle',
+        '“The only way to do great work is to love what you do.” – Steve Jobs',
+        '“If you’re offered a seat on a rocket ship, don’t ask what seat! Just get on.” – Sheryl Sandberg',
+        '“Work to become, not to acquire.” – Elbert Hubbard',
+        '“The future depends on what you do today.” – Mahatma Gandhi',
+        '“Every day we have plenty of opportunities to get angry, stressed or offended. But what you’re doing when you indulge these negative emotions is giving something outside yourself power over your happiness. You can choose to not let little things upset you.” – Joel Osteen',
+        '“Opportunities don’t happen, you create them.” – Chris Grosser',
+        '“Don’t Let Yesterday Take Up Too Much Of Today.” – Will Rogers',
+        '“Do you want to know who you are? Don’t ask. Act! Action will delineate and define you.”  – Thomas Jefferson',
+        '“The way to get started is to quit talking and begin doing.” – Walt Disney',
+        '“Start by doing what is necessary, then what is possible, and suddenly you are doing the impossible.” – Francis of Assisi',
+        '“Nothing will work unless you do.” – Maya Angelou',
+        '“Your only limit is you.” – Unknown',
+        '“The journey of a thousand miles begins with one step.” – Lao Tzu'
     ],
     weekendQuotes:[
-        '“You only live once, but if you do it right, once is enough.” — Mae West',
-        '“Many of life’s failures are people who did not realize how close they were to success when they gave up.” - Thomas A. Edison',
-        '“If you want to live a happy life, tie it to a goal, not to people or things.”– Albert Einstein',
-        '“Never let the fear of striking out keep you from playing the game.”– Babe Ruth'        
+        '“I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.” – Michael Jordan',
+        '“Strive not to be a success, but rather to be of value.” – Albert Einstein',
+        '“Everything you want is on the other side of fear.” – Jack Canfield',
+        '“If opportunity doesn’t knock, build a door.” – Milton Berle',
+        '“The more you talk about negative things in your life, the more you call them in. Speak victory not defeat.” – Joel Osteen',
+        '“I have learned over the years that when one’s mind is made up, this diminishes fear.” – Rosa Parks',
+        '“The question isn’t who is going to let me; it’s who is going to stop me.” – Ayn Rand',
+        '“The road to success is always under construction” – Lily Tomlin',
+        '“Build your own dreams, or someone else will hire you to build theirs.” – Farrah Gray',
+        '“The future belongs to those who believe in the beauty of their dreams.” – Eleanor Roosevelt',
+        '“I am always doing that which I cannot do, in order that I may learn how to do it.” – Pablo Picasso',
+        '“I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do.” – Leonardo da Vinci',
+        '“The best way to predict the future is to create it.” – Abraham Lincoln',
+        '“It is never too late to be what you might have been.” – George Eliot',
+        '“A mind troubled by doubt cannot focus on the course to victory.” – Arthur Golden',
+        '“There is a winner in you. You were created to be successful, to accomplish your goals, to leave your mark on this generation. You have greatness in you. The key is to get it out.” – Joel Osteen',
+        '“I alone cannot change the world, but I can cast a stone across the water to create many ripples.” – Mother Teresa',
+        '“When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one that has been opened for us.” – Helen Keller',
+        '“Success is walking from failure to failure with no loss of enthusiasm.” – Winston Churchill',
+        '“Whenever you see a successful person you only see the public glories, never the private sacrifices to reach them.” – Vaibhav Shah',
+        '“Innovation distinguishes between a leader and a follower.” – Steve Jobs',
+        '“Life is never easy. There is work to be done and obligations to be met – obligations to truth, to justice, and to liberty.” — John F. Kennedy (JFK Quotes)',
+        '“Live for each second without hesitation.” — Elton John',
+        '“Life is like riding a bicycle. To keep your balance, you must keep moving.” — Albert Einstein',
+        '“Life is a succession of lessons which must be lived to be understood.” — Helen Keller',
+        '“Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.” — Steve Jobs',
+        '“My mama always said, life is like a box of chocolates. You never know what you’re gonna get.” — Forrest Gump',
+        '“The healthiest response to life is joy.” — Deepak Chopra',
+        '“Life is like a coin. You can spend it any way you wish, but you only spend it once.” — Lillian Dickson',
+        '“The best portion of a good man’s life is his little nameless, unencumbered acts of kindness and of love.” — Wordsworth',
+        '“Life is ten percent what happens to you and ninety percent how you respond to it.” — Charles Swindoll',
+        '“Maybe that’s what life is… a wink of the eye and winking stars.” — Jack Kerouac',
+        '“Life is a flower of which love is the honey.” — Victor Hugo',
+        '“Health is the greatest gift, contentment the greatest wealth, faithfulness the best relationship.” — Buddha'
     ],
     
     tuesToFriQuotes:[
-        '“Money and success don’t change people; they merely amplify what is already there.” — Will Smith',
-        '“Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking.” – Steve Jobs',
-        '“Not how long, but how well you have lived is the main thing.” — Seneca',
-        '“If life were predictable it would cease to be life, and be without flavor.” – Eleanor Roosevelt'
+        '“My mother always used to say: The older you get, the better you get, unless you’re a banana.” — Rose (Betty White), The Golden Girls',
+        'I’m sick of following my dreams, man. I’m just going to ask where they’re going and hook up with ’em later. — Mitch Hedberg',
+        'A pessimist is a person who has had to listen to too many optimists. —Don Marquis',
+        '“If life were predictable it would cease to be life, and be without flavor.” – Eleanor Roosevelt',
+        '“The purpose of our lives is to be happy.” — Dalai Lama',
+        '“Life is what happens when you’re busy making other plans.” — John Lennon',
+        '“Get busy living or get busy dying.” — Stephen King',
+        '“You only live once, but if you do it right, once is enough.” — Mae West',
+        '“Many of life’s failures are people who did not realize how close they were to success when they gave up.” - Thomas A. Edison',
+        '“If you want to live a happy life, tie it to a goal, not to people or things.”– Albert Einstein',
+        '“Never let the fear of striking out keep you from playing the game.”– Babe Ruth',
+        '“Knowing trees, I understand the meaning of patience. Knowing grass, I can appreciate persistence.” – Hal Borland',
+        '“Everybody wants to be famous, but nobody wants to do the work. I live by that. You grind hard so you can play hard. At the end of the day, you put all the work in, and eventually it’ll pay off. It could be in a year, it could be in 30 years. Eventually, your hard work will pay off.” — Kevin Hart',
+        '“Everything negative – pressure, challenges – is all an opportunity for me to rise.” — Kobe Bryant',
+        '“I like criticism. It makes you strong.” — LeBron James',
+        'You never really learn much from hearing yourself speak.” ― George Clooney',
+        '“Life imposes things on you that you can’t control, but you still have the choice of how you’re going to live through this.” — Celine Dion',
+        '“When we do the best we can, we never know what miracle is wrought in our life or the life of another.” — Helen Keller',
+        '“Keep calm and carry on.” — Winston Churchill',
+        '“You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.” — Dr. Seuss',
+        'The best thing about the future is that it comes one day at a time. — Abraham Lincoln',
+        '“Do not take life too seriously. You will never get out of it alive.” — Elbert Hubbard'
     ],
     
     activities:[
@@ -98,7 +172,7 @@ const zodQuote = {
     },
 
     //generating quotes based on what day's now
-    generateQuotesFromDay(){
+    generateQuotesByDay(){
         let quotes = ''
         if(this.generateDay() == "Saturday" || this.generateDay() == "Sunday"){
             quotes = this.generateData(zodQuote.weekendQuotes);
@@ -166,7 +240,7 @@ const zodQuote = {
 
 zodQuote.generateFullDate()
 console.log("\n")
-zodQuote.generateQuotesFromDay()
+zodQuote.generateQuotesByDay()
 console.log("\n")
 zodQuote.printZodiacsMessage();
 
